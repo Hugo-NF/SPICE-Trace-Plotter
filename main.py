c68fdc36df5b9ec3b103d61f61c52cc8dab07bc5
Hugo-NF <hugonfonseca@hotmail.com>
2020-10-16 20:02:26 -0300

    Add config wizard, unified legend

--- main.py
+++ main.py
@@ -17,14 +17,20 @@ arg_parser.add_argument('path',
 
 arg_parser.add_argument('output',
                        metavar='output',
+                       default='output.png',
                        type=str,
                        help='the path to output file')
+
+arg_parser.add_argument('title',
+                       metavar='title',
+                       type=str,
+                       help='chart title')
+
 
 # Execute the parse_args() method
 args = arg_parser.parse_args()
 
 input_file = args.path
-output_file = args.output
 
 if not os.path.exists(input_file):
     print("Input file %s does NOT exist" % input_file)
@@ -34,4 +40,34 @@ file_stream = open(input_file, 'r', encoding='utf8')
 
 data = Parser.parse(file_stream)
 
-Plotter.plot(data)
+x_label = input('Label do eixo X: ')
+y_label = input('Label do eixo Y: ')
+
+scales = input('Duas escalas? (y/n)')
+right_y_label = ''
+if scales == 'y':
+    right_y_label = input('Label da segunda escala: ')
+
+options = {
+    'output': args.output,
+    'title': args.title,
+    'x_label': x_label,
+    'y_label': y_label,
+    'scales': scales,
+    'right_y_label': right_y_label,
+    'traces': {}
+}
+
+for trace in data.keys():
+    if trace != 'time':
+        print('Trace: %s' % trace)
+        plot = input('Plot? (y/n)')
+        if plot == 'y':
+            options['traces'][trace] = {
+                'scale': input('Segunda escala? (y/n)') if scales == 'y' else 'n',
+                'label': input('Legenda'),
+                'color': input('Cor'),
+                'marker': input('Marcador')
+            }
+
+Plotter.plot(data, options)
--- plotter.py
+++ plotter.py
@@ -1,26 +1,45 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 
 class Plotter:
     @staticmethod
-    def plot(data, options=None):
+    def plot(data, options):
         fig, ax1 = plt.subplots()
-
-        ax1.set_xlabel('Tempo (s)')
-        ax1.set_ylabel('Tensão (V)')
-        ax1.plot(data['time'], data['V(vin)'], marker='', color='blue', linewidth=1, alpha=0.9, label='Vin')
-        ax1.plot(data['time'], data['V(vout)'], marker='', color='green', linewidth=1, alpha=0.9, label='Vout')
+        plots = []
+        ax1.set_xlabel(options['x_label'])
+        ax1.set_ylabel(options['y_label'])
+        for trace in options['traces'].keys():
+            if options['traces'][trace]['scale'] == 'n':
+                plots.append(ax1.plot(data['time'],
+                                      data[trace],
+                                      marker=options['traces'][trace]['marker'],
+                                      color=options['traces'][trace]['color'],
+                                      linewidth=1,
+                                      alpha=0.9,
+                                      label=options['traces'][trace]['label']))
         ax1.tick_params(axis='y')
-        ax1.legend(loc=1)
-        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
-        ax2.set_ylabel('Corrente (A)')  # we already handled the x-label with ax1
-        ax2.plot(data['time'], data['I(R1)'], marker='', color='red', linewidth=1, alpha=0.9, label='I(R1)')
-        ax2.tick_params(axis='y')
+        if options['scales'] == 'y':
+            ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+            ax2.set_ylabel(options['right_y_label'])  # we already handled the x-label with ax1
+            for trace in options['traces'].keys():
+                if options['traces'][trace]['scale'] == 'y':
+                    plots.append(ax2.plot(data['time'],
+                                          data[trace],
+                                          marker=options['traces'][trace]['marker'],
+                                          color=options['traces'][trace]['color'],
+                                          linewidth=1,
+                                          alpha=0.9,
+                                          label=options['traces'][trace]['label']))
+            ax2.tick_params(axis='y')
 
-        fig.tight_layout(rect=[0, 0.03, 1, 0.95])  # otherwise the right y-label is slightly clipped
-        ax2.legend(loc=2)
-        plt.title('Conversor Boost')
-        fig.savefig('boost.png')
+            fig.tight_layout(rect=[0, 0.03, 1, 0.95])  # otherwise the right y-label is slightly clipped
+
+        plt.title(options['title'])
+        fig.savefig(options['output'])
+        plt_sum = []
+        for plot in plots:
+            plt_sum += plot
+        labels = [label.get_label() for label in plt_sum]
+        ax1.legend(plt_sum, labels, loc=0)
         plt.show()
