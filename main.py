026e43746594830ed6b3786768e99409efa5f0bf
Hugo-NF <hugonfonseca@hotmail.com>
2020-11-22 11:14:48 -0300

    Move configuration to .json file

--- main.py
+++ main.py
@@ -1,72 +1,23 @@
 import os
-import argparse
+import json
 
 from parser import Parser
 from plotter import Plotter
 
-# Create the parser
-arg_parser = argparse.ArgumentParser(description='Plot charts from SPICE text results')
-
-# Add the arguments
-arg_parser.add_argument('path',
-                        metavar='path',
-                        type=str,
-                        help='the path to SPICE text results')
-
-arg_parser.add_argument('output',
-                        metavar='output',
-                        default='output.png',
-                        type=str,
-                        help='the path to output file')
-
-arg_parser.add_argument('title',
-                        metavar='title',
-                        type=str,
-                        help='chart title')
-
-# Execute the parse_args() method
-args = arg_parser.parse_args()
-
-input_file = args.path
-
-if not os.path.exists(input_file):
-    print("Input file %s does NOT exist" % input_file)
+try:
+    with open('settings.json', 'r') as settings_file:
+        options = json.load(settings_file)
+        print(options)
+except FileNotFoundError:
+    print('Missing configuration file: settings.json. Nothing to do...')
     exit(-1)
 
-file_stream = open(input_file, 'r', encoding='utf8')
+if not os.path.exists(options['input']):
+    print("Input file %s does NOT exist" % options['input'])
+    exit(-1)
+
+file_stream = open(options['input'], 'r', encoding='utf8')
 
 data = Parser.parse(file_stream)
-
-options = {
-    'output': 'docs/boost.png',
-    'title': 'Conversor Boost',
-    'x_label': 'Tempo (s)',
-    'x_transform': 0.001,
-    'y_label': 'Tensão (V)',
-    'y_transform': 1,
-    'scales': 'y',
-    'right_y_label': 'Corrente (mA)',
-    'right_y_transform': 1000,
-    'traces': {
-        'V(vin)': {
-            'scale': 'n',
-            'label': 'Vin',
-            'color': 'blue',
-            'marker': ''
-        },
-        'V(vout)': {
-            'scale': 'n',
-            'label': 'Vout',
-            'color': 'green',
-            'marker': ''
-        },
-        'I(R1)': {
-            'scale': 'y',
-            'label': 'I(R1)',
-            'color': 'red',
-            'marker': ''
-        }
-    }
-}
 
 Plotter.plot(data, options)
--- plotter.py
+++ plotter.py
@@ -7,44 +7,42 @@ class Plotter:
     def plot(data, options):
         fig, ax1 = plt.subplots()
         plots = []
-        ax1.set_xlabel(options['x_label'])
-        ax1.set_ylabel(options['y_label'])
+        ax1.set_xlabel(options['x_axis']['label'])
+        ax1.set_ylabel(options['y_axis']['left']['label'])
 
-        scaler = lambda m: m * options['x_transform']
+        scaler = lambda m: m * options['x_axis']['transform']
         np_scaler = np.vectorize(scaler)
         data['time'] = np_scaler(data['time'])
 
-        for trace in options['traces'].keys():
-            if options['traces'][trace]['scale'] == 'n':
-                scaler = lambda m: m * options['y_transform']
+        for trace in options['traces']['left'].keys():
+            scaler = lambda m: m * options['y_axis']['left']['transform']
+            np_scaler = np.vectorize(scaler)
+            data[trace] = np_scaler(data[trace])
+
+            plots.append(ax1.plot(data['time'],
+                                  data[trace],
+                                  marker=options['traces']['left'][trace]['marker'],
+                                  color=options['traces']['left'][trace]['color'],
+                                  linewidth=1,
+                                  alpha=0.9,
+                                  label=options['traces']['left'][trace]['label']))
+        ax1.tick_params(axis='y')
+
+        if options['two_scales']:
+            ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+            ax2.set_ylabel(options['y_axis']['right']['label'])  # we already handled the x-label with ax1
+            for trace in options['traces']['right'].keys():
+                scaler = lambda m: m * options['y_axis']['right']['transform']
                 np_scaler = np.vectorize(scaler)
                 data[trace] = np_scaler(data[trace])
 
-                plots.append(ax1.plot(data['time'],
+                plots.append(ax2.plot(data['time'],
                                       data[trace],
-                                      marker=options['traces'][trace]['marker'],
-                                      color=options['traces'][trace]['color'],
+                                      marker=options['traces']['right'][trace]['marker'],
+                                      color=options['traces']['right'][trace]['color'],
                                       linewidth=1,
                                       alpha=0.9,
-                                      label=options['traces'][trace]['label']))
-        ax1.tick_params(axis='y')
-
-        if options['scales'] == 'y':
-            ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-            ax2.set_ylabel(options['right_y_label'])  # we already handled the x-label with ax1
-            for trace in options['traces'].keys():
-                if options['traces'][trace]['scale'] == 'y':
-                    scaler = lambda m: m * options['right_y_transform']
-                    np_scaler = np.vectorize(scaler)
-                    data[trace] = np_scaler(data[trace])
-
-                    plots.append(ax2.plot(data['time'],
-                                          data[trace],
-                                          marker=options['traces'][trace]['marker'],
-                                          color=options['traces'][trace]['color'],
-                                          linewidth=1,
-                                          alpha=0.9,
-                                          label=options['traces'][trace]['label']))
+                                      label=options['traces']['right'][trace]['label']))
             ax2.tick_params(axis='y')
 
             fig.tight_layout(rect=[0, 0.03, 1, 0.95])  # otherwise the right y-label is slightly clipped
